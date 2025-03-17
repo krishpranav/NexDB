6c619d33fce85009aae689c4393238a5e7116fea
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-17 15:38:16 +0530

    nex: util[ThreadUtil]

--- src/main/java/org/nex/util/ThreadUtil.java
+++ src/main/java/org/nex/util/ThreadUtil.java
@@ -5,12 +5,17 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
-public class ThreadUtil {
+public final class ThreadUtil {
+
     private ThreadUtil() { }
 
     public static Future doInBackground(Callable callable) {
         final ExecutorService executor = Executors.newSingleThreadExecutor();
         Future future = executor.submit(callable);
+
+        if(executor.isTerminated()) {
+            executor.shutdown();
+        }
 
         return future;
     }
