4052b09f462acdfb67357355a57632df67babace
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-17 16:05:14 +0530

    nex: util[MigrationFileParser]

---
+++ src/main/java/org/nex/util/MigrationFileParser.java
@@ -0,0 +1,15 @@
+package org.nex.util;
+
+public class MigrationFileParser {
+
+    private String content;
+    
+    public MigrationFileParser(String content){
+        this.content = content.replaceAll("(\\/\\*([\\s\\S]*?)\\*\\/)|(--(.)*)|(\n)","");
+    }
+
+    public String[] getStatements(){
+        return this.content.split(";");
+    }
+
+}
