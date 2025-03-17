27c26d3f83ff63f3efec3a155939cbec4b734ae4
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-17 16:15:52 +0530

    nex: NexContext

--- src/main/java/org/nex/NexContext.java
+++ src/main/java/org/nex/NexContext.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import java.util.WeakHashMap;
 
 public class NexContext {
+
     private static NexDbConfiguration dbConfiguration = null;
     private static NexContext instance = null;
     private NexDb nexDb;
@@ -19,7 +20,7 @@ public class NexContext {
 
     public static NexContext getNexContext() {
         if (instance == null) {
-            throw new NullPointerException("");
+            throw new NullPointerException("NexContext has not been initialized properly. Call NexContext.init(Context) in your Application.onCreate() method and NexContext.terminate() in your Application.onTerminate() method.");
         }
 
         return instance;
@@ -31,18 +32,35 @@ public class NexContext {
         dbConfiguration = null;
     }
 
-
     public static void init(Context context, NexDbConfiguration configuration) {
         init(context);
         dbConfiguration = configuration;
     }
 
+
     public static void terminate() {
         if (instance == null) {
             return;
         }
-
         instance.doTerminate();
         ContextUtil.terminate();
+    }
+
+    private void doTerminate() {
+        if (this.nexDb != null) {
+            this.nexDb.getDB().close();
+        }
+    }
+
+    public static NexDbConfiguration getDbConfiguration() {
+        return dbConfiguration;
+    }
+
+    public NexDb getNexDb() {
+        return nexDb;
+    }
+
+    public Map<Object, Long> getEntitiesMap() {
+        return entitiesMap;
     }
 }
