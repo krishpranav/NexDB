6db812bb6ed12b6d7ef50f8449b21a65db772e4c
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-17 16:23:59 +0530

    nex: helper[ManifestHelper]

--- src/main/java/org/nex/helper/ManifestHelper.java
+++ src/main/java/org/nex/helper/ManifestHelper.java
@@ -9,7 +9,7 @@ public final class ManifestHelper {
     private static final String LOG_TAG = "Nex";
     private static Boolean debugEnabled = null;
 
-    public final String METADATA_DATABASE = "DATABASE";
+    public final static String METADATA_DATABASE = "DATABASE";
 
 
     public final static String METADATA_VERSION = "VERSION";
@@ -19,4 +19,48 @@ public final class ManifestHelper {
     public final static String DATABASE_DEFAULT_NAME = "Nex.db";
 
     private ManifestHelper() { }
+
+    public static int getDatabaseVersion() {
+        Integer databaseVersion = getMetaDataInteger(METADATA_VERSION);
+
+        if ((databaseVersion == null) || (databaseVersion == 0)) {
+            databaseVersion = 1;
+        }
+
+        return databaseVersion;
+    }
+
+    public static String getDatabaseName() {
+        String databaseName = getMetadataString(METADATA_DATABASE);
+
+        if (databaseName == null) {
+            databaseName = DATABASE_DEFAULT_NAME;
+        }
+
+        return databaseName;
+    }
+
+    public static String getDbName() {
+        return getDatabaseName();
+    }
+
+    private static String getMetadataString(String name) {
+        PackageManager pm = getPackageManager();
+        String value = null;
+
+        try {
+            ApplicationInfo ai = pm.getApplicationInfo(getPackageName(), PackageManager.GET_META_DATA);
+            value = ai.metaData.getString(name);
+        } catch (Exception e) {
+            if (ManifestHelper.isDebugEnabled()) {
+                Log.d(LOG_TAG, "Couldn't find config value: " + name);
+            }
+        }
+
+        return value;
+    }
+
+    private static Integer getMetaDataInteger(String name) {
+        PackageManager pm = getPackageManager();
+    }
 }
